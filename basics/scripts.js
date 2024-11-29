f03be14e264e817fe32508fd96df8383502ce55f
Kortvelyesi Roland <kortvelyesi.roland@BOLYAI.LOCAL>
2024-11-29 12:58:34 +0100

    ugras

--- basics/scripts.js
+++ basics/scripts.js
@@ -1,25 +1,53 @@
-function Player(nickname,) {
-    this.nickname = nickname
-    this.playedMatch = 0
-}
-Player.prototype.play = function() {
-    this.playedMatch++;
-    console.log(this.nickname, this.playedMatch)
-}
+// function Player(nickname,) {
+//     this.nickname = nickname
+//     this.playedMatch = 0
+// }
+// Player.prototype.play = function() {
+//     this.playedMatch++;
+//     console.log(this.nickname, this.playedMatch)
+// }
 
-Player.prototype.getTierLevel = function() {
-    if(this.playedMatch <= 3){
-        return 'a'
+// Player.prototype.getTierLevel = function() {
+//     if(this.playedMatch <= 3){
+//         return 'a'
+//     }
+//     else if(this.playedMatch < 3 && this.playedMatch <= 5) {
+//         return 'b'
+//     }
+//     else{
+//         return 'c'
+//     }
+// }
+class Player{
+    constructor(nickname) {
+        this.nickname = nickname
+        this.playedMatch = 0
     }
-    else if(this.playedMatch < 3 && this.playedMatch <= 5) {
-        return 'b'
+
+    play() {
+        this.playedMatch++;
+        console.log(this.nickname, this.playedMatch)
     }
-    else{
-        return 'c'
+
+    getTierLevel() {
+        Player.prototype.getTierLevel = function() {
+            if(this.playedMatch <= 3){
+                return 'a'
+            }
+            else if(this.playedMatch < 3 && this.playedMatch <= 5) {
+                return 'b'
+            }
+            else{
+                return 'c'
+            }
+         }
     }
 }
 
 const gomszab = new Player("gomszab")
+
 console.log(gomszab)
 gomszab.play()
 console.log(gomszab.getTierLevel());
+
+
