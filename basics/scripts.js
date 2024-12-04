91d5f6dc71d1bb7d211645afd7dcdc4f87a472b2
Roli <rolandkortvelyesi12@gmail.com>
2024-12-04 21:10:04 +0100

    kommentelve

--- basics/scripts.js
+++ basics/scripts.js
@@ -50,56 +50,56 @@ console.log(gomszab)
 gomszab.play()
 console.log(gomszab.getTierLevel());
 
-function Person(name) {
-    this.name = name;
-}
+// function Person(name) {
+//     this.name = name;
+// }
 
-Person.prototype.getName = function() {
-    return this.name;
-};
+// Person.prototype.getName = function() {
+//     return this.name;
+// };
 
-function Student(name, school) {
-    Person.call(this, name);
-    this.school = school;
-}
+// function Student(name, school) {
+//     Person.call(this, name);
+//     this.school = school;
+// }
 
-Object.setPrototypeOf(Student.prototype, Person.prototype);
+// Object.setPrototypeOf(Student.prototype, Person.prototype);
 
-const student = new Student("Géza", "Bolyai");
-console.log("Név: " + student.getName() + " , Iskola: " + student.school);
+// const student = new Student("Géza", "Bolyai");
+// console.log("Név: " + student.getName() + " , Iskola: " + student.school);
 
-function Animal(name) {
-    this.name = name;
-}
+// function Animal(name) {
+//     this.name = name;
+// }
 
-Animal.prototype.hang = function() {
-    console.log("A", this.name + " hangot ad ki");
-};
+// Animal.prototype.hang = function() {
+//     console.log("A", this.name + " hangot ad ki");
+// };
 
-function Mammal(name) {
-    Animal.call(this, name); 
-}
+// function Mammal(name) {
+//     Animal.call(this, name); 
+// }
 
-Object.setPrototypeOf(Mammal.prototype, Animal.prototype);
+// Object.setPrototypeOf(Mammal.prototype, Animal.prototype);
 
-Mammal.prototype.jar = function() {
-    console.log("A", this.name + " jár");
-};
+// Mammal.prototype.jar = function() {
+//     console.log("A", this.name + " jár");
+// };
 
-function Bird(name) {
-    Animal.call(this, name);  
-}
+// function Bird(name) {
+//     Animal.call(this, name);  
+// }
 
-Object.setPrototypeOf(Bird.prototype, Animal.prototype);
+// Object.setPrototypeOf(Bird.prototype, Animal.prototype);
 
-Bird.prototype.repul = function() {
-    console.log("A", this.name + " repül");
-};
+// Bird.prototype.repul = function() {
+//     console.log("A", this.name + " repül");
+// };
 
-const mammal = new Mammal("Kutya");
-mammal.hang(); 
-mammal.jar(); 
+// const mammal = new Mammal("Kutya");
+// mammal.hang(); 
+// mammal.jar(); 
 
-const bird = new Bird("Veréb");
-bird.hang();   
-bird.repul();
+// const bird = new Bird("Veréb");
+// bird.hang();   
+// bird.repul();
