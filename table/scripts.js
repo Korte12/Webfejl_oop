43c4855b55a9a99d6fa50a421314d57915f3ee51
Kortvelyesi Roland <kortvelyesi.roland@BOLYAI.LOCAL>
2024-12-13 12:38:58 +0100

    orai moka

--- table/scripts.js
+++ table/scripts.js
@@ -19,3 +19,53 @@ const array = [
         lastname: 'Horváth'
     },
 ]
+
+class Person {
+    constructor(obj) {
+        this.firstname1 = obj.firstname1
+        this.firstname2 = obj.firstname2
+        this.lastname = obj.lastname
+    }
+    render(parentElement) {
+        const row = document.createElement('tr')
+        parentElement.appendChild(row)
+
+        const firstn1 = document.createElement('td')
+        firstn1.innerHTML = this.firstname1
+
+        const firstn2 = document.createElement('td')
+        firstn2.innerHTML = this.firstname2
+
+        const lastn = document.createElement('td')
+        lastn.innerHTML = this.lastname
+        row.appendChild(lastn)
+        row.appendChild(firstn1)
+
+        if(this.firstname2 == undefined) {
+            this.colSpan = 2
+        }
+        else {
+            const firstn2 = document.createElement('td')
+            firstn2.innerHTML = this.firstname2
+            row.appendChild(firstn2)
+            
+        }
+        parentElement.appendChild(row)
+        
+
+
+    }
+
+}
+
+function init() {
+    const tbody = document.getElementById('tbodyId')
+    for (const i of array) {
+        const ember = new Person(i);
+        ember.render(tbody);
+
+
+    }
+}
+
+init();
